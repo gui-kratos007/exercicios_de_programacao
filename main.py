bf8700c2a77ebf29989c7e69edec363b86d3f56e
gui-kratos007 <gui.souza2003oliveir@gmail.com>
2023-09-15 17:49:57 -0300

    Código referente à task #30 do exercício #27 do projeto treinamento no taiga.io

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ def contar_palavras(file_path):
     return "arquivo vazio"
 
 
+def frequencia():
+    text = ("Na vida, a perseverança é essencial. Perseverança nos desafios, perseverança nas conquistas. Com "
+            "dedicação e perseverança, alcançamos nossos objetivos. A perseverança nos impulsiona, nos inspira e nos "
+            "torna resilientes diante das adversidades.").replace(",", "").replace(".", "").lower()
+    # print(text.split().count("que"))
+    max_min = [(name, text.split().count(name)) for name in text.split()]
+    print(max_min)
+    """new_list = []
+    for name in text.split():
+        new_list.append(name)
+        #print(f"{name} ({text.split().count(name)})")"""
+
+
 if __name__ == "__main__":
-    file_path = input("Entre com o path do arquivo que deseja processar: ")
+    """file_path = input("Entre com o path do arquivo que deseja processar: ")
     numero_de_palavras = contar_palavras(file_path)
-    print(numero_de_palavras)
+    print(numero_de_palavras)"""
+
+    frequencia()
+
