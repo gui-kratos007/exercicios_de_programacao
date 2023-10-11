9995df9a1257ea5f6a322eeb10cadcad0842e8dd
gui-kratos007 <gui.souza2003oliveir@gmail.com>
2023-10-11 16:10:04 -0300

    Acertei a maioria das coisas pedidas no github, como diminuir tamanho de algumas funções, criando outras se necessário; colocar a lógica e as ações das opções do menu em funções; etc mas ainda falta uma das coisas pedidas para resolver

--- close_words.py
+++ close_words.py
@@ -1,5 +1,16 @@
 import random
 
+def list_the_words(path):
+    """
+    lê o texto e retorna a lista de palavras dele
+    :param path: caminho do arquivo
+    :return: lista de palavras do texro
+    """
+    text = get_text(path)
+    list_words = text_clean(text).split()
+    return list_words
+
+
 def get_text(path):
     """
     Esta função lê o arquivo e retorna o seu conteúdo, mas caso não exista ele retorna uma mensagem de erro.
@@ -51,6 +62,31 @@ def search_word(txt):
     return "essa palavra não está no texto"
 
 
+def ascending_alphabetical_order(list_words):
+    """
+    ordena as palavras em ordem alfabética crescente
+    :param list_words: lista de palavras do texto
+    :return: as palavras ordenadas em ordem alfabética crescente
+    """
+    word_sorted = [word for word in set(list_words)]
+    return sorted(word_sorted)
+
+
+def descending_alphabetical_order(list_words):
+    """
+        ordena as palavras em ordem alfabética decrescente
+        :param list_words: lista de palavras do texto
+        :return: as palavras ordenadas em ordem alfabética decrescente
+        """
+    word_sorted = [word for word in set(list_words)]
+    return sorted(word_sorted, reverse=True)
+
+
+def calculate_percentage(ocurrences, total):
+    percentage_value = (ocurrences / total) * 100
+    return percentage_value
+
+
 def print_percentage(dict1, dict2):
     """
     imprime as porcentagens de aparições das palavras de cada dict
@@ -64,13 +100,13 @@ def print_percentage(dict1, dict2):
     print("Porcentagem da frequencia dos anteriores: \n")
     for item in dict1:
         ocurrences = dict1.get(item, 0)
-        percentage_value = (ocurrences / total_itens) * 100
+        percentage_value = calculate_percentage(ocurrences, total_itens)
         print(item, "(", percentage_value, "%) \n")
     print('-' * 150)
     print("Porcentagem da frequencia dos posteriores: \n")
     for item in dict2:
         ocurrences = dict2.get(item, 0)
-        percentage_value = (ocurrences / total_itens2) * 100
+        percentage_value = calculate_percentage(ocurrences, total_itens2)
         print(item, "(", percentage_value, "%) \n")
     return 0
 
@@ -85,14 +121,14 @@ def desempatar(lista):
     return escolha
 
 
-"""def print_most_commons(lista1, lista2, word):
-    ""
+def print_most_commons(lista1, lista2, word):
+    """"
     imprime a frase mais provável
     :param lista1: lista das anteriores mais freuquentes
     :param lista2: lista das posteriores mais freuquentes
     :param word: palavra escolhida pelo usuário
     :return: frase mais provável
-    ""
+    """
     print("A frase mais provável é: ")
     if len(lista1) > 1 and len(lista2) > 1:
         immediate_previous = desempatar(lista1)
@@ -107,7 +143,7 @@ def desempatar(lista):
         immediate_subsequent = desempatar(lista2)
         return f"{immediate_previous} {word} {immediate_subsequent}"
     else:
-        return f"{lista1[0]} {word} {lista2[0]}" """
+        return f"{lista1[0]} {word} {lista2[0]}"
 
 
 def check_tie(dict1, dict2, lista):
@@ -132,7 +168,6 @@ def check_tie(dict1, dict2, lista):
         print(previous_word_tie, "\n")
         print("Lista das mais frequentes posteriores:")
         print(subsequent_word_tie, "\n")
-        print('-' * 150, "\n")
     return "não tem nenhuma palavra nas listas"
 
 
@@ -153,6 +188,55 @@ def frequency(dict1, dict2, lista):
     return "não tem palavras nas listas"
 
 
+def add_in_list(lista, word, words):
+    """
+    adiciona itens em uma lista se esse item for igual a palavra escolhida pelo usuário.
+    :param lista: lista em que os itens serão adicionados
+    :param word: palavra escolhida pelo usuário
+    :param words: lista de palavras do documento
+    :return: tamanho da lista
+    """
+    for i, item in enumerate(words):
+        if item == word:
+            lista.append(i)
+    return len(lista)
+
+
+def add_itens_in_dicts(func, dict1, dict2, word, words):
+    """
+    se existir ao menos 1 item na lista, adiciona esse item na sua respectiva dict e atribui
+     valor a esses itens, que aumenta a medida que ele reaparece na contagem da lista
+    :param func: função que retorna o tamanho da lista
+    :param dict1: dicionário de palavras anteriores, que no início está vazio
+    :param dict2: dicionário de palavras posteriores, que no início está vazio
+    :param word: palavra escolhida pelo usuário
+    :param words: lista de palavras
+    :return: dicionarios de anteriores e posteriores, com os seus respectivos itens e valores,
+    que agora não estará mais vazio, como no início
+    """
+    if func > 0:
+        # Conta a frequencia das palavras anteriores e posteriores
+        for j, item in enumerate(words):
+            if item == word:
+                previous_word = words[j - 1]
+                subsequent_word = words[j + 1]
+
+                # Se a palavra já estiver no dict das anteriores, adiciona mais 1 ao seu valor
+                if previous_word in dict1:
+                    dict1[previous_word] += 1
+                # Se a palavra não estiver no dict das anteriores, adiciona ela e coloque seu valor como 1.
+                else:
+                    dict1[previous_word] = 1
+
+                # Se a palavra já estiver no dict das posteriores, adiciona mais 1 ao seu valor
+                if subsequent_word in dict2:
+                    dict2[subsequent_word] += 1
+                # Se a palavra não estiver no dict das posteriores, adiciona ela e coloque seu valor como 1.
+                else:
+                    dict2[subsequent_word] = 1
+    return dict1, dict2
+
+
 def show_all(txt):
     """
     Calcula a frequencia de aparições das palavras
@@ -165,31 +249,8 @@ def show_all(txt):
     previous = {}
     subsequent = {}
 
-    for i, item in enumerate(words):
-        if item == word:
-            lista_de_busca.append(i)
-
-    if len(lista_de_busca) > 0:
-        # Conta a frequencia das palavras anteriores e posteriores
-        for j, item in enumerate(words):
-            if item == word:
-                previous_word = words[j - 1]
-                subsequent_word = words[j + 1]
-
-                # Se a palavra já estiver no dict das anteriores, adiciona mais 1 ao seu valor
-                if previous_word in previous:
-                   previous[previous_word] += 1
-                # Se a palavra não estiver no dict das anteriores, adiciona ela e coloque seu valor como 1.
-                else:
-                    previous[previous_word] = 1
-
-                # Se a palavra já estiver no dict das posteriores, adiciona mais 1 ao seu valor
-                if subsequent_word in subsequent:
-                    subsequent[subsequent_word] += 1
-                # Se a palavra não estiver no dict das posteriores, adiciona ela e coloque seu valor como 1.
-                else:
-                    subsequent[subsequent_word] = 1
-
+    list_add = add_in_list(lista_de_busca, word, words)
+    add_itens_in_dicts(list_add, previous, subsequent, word, words)
     frequency(previous, subsequent, lista_de_busca)
     print_percentage(previous, subsequent)
     check_tie(previous, subsequent, lista_de_busca)
--- menu.py
+++ menu.py
@@ -16,16 +16,12 @@ def menu():
 
         try:
             answer = int(input("Digite sua opção: "))
+            path = input("Entre com o path do arquivo que deseja processar: ")
             if answer == 1:
-                path = input("Entre com o path do arquivo que deseja processar: ")
-                option_1(path)
+                answer_1(path)
             elif answer == 2:
-                path = input("Entre com o path do arquivo que deseja processar: ")
-                text = get_text(path)
-                list_words = text_clean(text).split()
-                print()
-                print(list_words)
-                print()
+                list_the_words(path)
+
                 print("-" * 5, "Escolha uma opção para interagir com essa lista", "-" * 5)
                 print("Opções:")
                 print("[1] -> Ordenar por palavras mais frequentes")
@@ -38,21 +34,13 @@ def menu():
                 option = int(input("Digite sua opção: "))
                 print()
                 if option == 1:
-                    words_count = [[word, list_words.count(word)] for word in set(list_words)]
-                    order_words = sorted(words_count, key=get_count, reverse=True)
-                    for word, count in order_words:
-                        print(f"{word} ({count})")
+                    option_1(list_the_words(path))
                 elif option == 2:
-                    words_count = [[word, list_words.count(word)] for word in set(list_words)]
-                    order_words = sorted(words_count, key=get_count)
-                    for word, count in order_words:
-                        print(f"{word} ({count})")
+                    option_2(list_the_words(path))
                 elif option == 3:
-                    ordem_alfabetica_crescente = ascending_alphabetical_order(list_words)
-                    print(ordem_alfabetica_crescente)
+                    option_3(list_the_words(path))
                 elif option == 4:
-                    ordem_alfabetica_decrescente = descending_alphabetical_order(list_words)
-                    print(ordem_alfabetica_decrescente)
+                    option_4(list_the_words(path))
                 elif option == 5:
                     continue
                 elif option == 6:
@@ -62,16 +50,9 @@ def menu():
                     print("Opção inválida. Volte do ínicio e tente novamente com um número válido.")
                 print()
             elif answer == 3:
-                path = input("Entre com o path do arquivo que deseja processar: ")
-                text = get_text(path)
-                search = search_word(text)
-                print()
-                print(search)
-                print()
+                answer_3(path)
             elif answer == 4:
-                path = input("Entre com o path do arquivo que deseja processar: ")
-                text = get_text(path)
-                show_all(text)
+                answer_4(path)
             elif answer == 9:
                 print("Programa finalizado!")
                 break
@@ -80,7 +61,8 @@ def menu():
         except ValueError:
             print("Você não digitou um número inteiro, tente novamente.")
 
-def option_1(path):
+
+def answer_1(path):
     """
     lê o arquivo e retorna o conteúdo dele
     :param path: caminho do arquivo
@@ -90,6 +72,81 @@ def option_1(path):
     print("\n O arquivo foi carregado, veja abaixo o seu conteúdo: ")
     return print(text)
 
+
+def option_1(func):
+    """
+    cria uma lista de palavras com o seu numero de aparições, as ordena e retorna elas ordenadas em ordem decrescente
+    :param func: função da lista de palavras
+    :return: palavras seguidas da sua quantidade de aparições em ordem decrescente de aparições.
+    """
+    words_count = [[word, func.count(word)] for word in set(func)]
+    order_words = sorted(words_count, key=get_count, reverse=True)
+    for word, count in order_words:
+        print(f"{word} ({count})")
+
+
+def option_2(func):
+    """
+    cria uma lista de palavras com o seu numero de aparições, as ordena e retorna elas ordenadas em ordem crescente
+    :param func: função da lista de palavras
+    :return: palavras seguidas da sua quantidade de aparições em ordem crescente de aparições.
+    """
+    words_count = [[word, func.count(word)] for word in set(func)]
+    order_words = sorted(words_count, key=get_count)
+    for word, count in order_words:
+        print(f"{word} ({count})")
+
+
+def option_3(func):
+    """
+    coloca a lista de palavras em ordem alfabética crescente e retorna a impressão delas
+    :param func: lista de palavras
+    :return: lista de palavras em ordem alfabética crescente
+    """
+    ordem_alfabetica_crescente = ascending_alphabetical_order(func)
+    return print(ordem_alfabetica_crescente)
+
+
+def option_4(func):
+    """
+    coloca a lista de palavras em ordem alfabética decrescente e retorna a impressão delas
+    :param func: lista de palavras
+    :return: lista de palavras em ordem alfabética decrescente
+    """
+    ordem_alfabetica_decrescente = descending_alphabetical_order(func)
+    return print(ordem_alfabetica_decrescente)
+
+
+def answer_3(path):
+    """
+    faz uma pesquisa da palavra que o usuário escolher e retorna o(s) índice(s) que ela aparece no texto
+    :param path: caminho do arquivo
+    :return: indice(s) da palavra escolhida
+    """
+    text = get_text(path)
+    search = search_word(text)
+    return print("\n", search, "\n")
+
+
+def answer_4(path):
+    """
+    pega o texto e retorna todas os dados de frequencia da palavra que o usuário escolheu
+    :param path: caminho do arquivo
+    :return: todos os dados da frequencia da palavra que o usuário escolheu.
+    """
+    text = get_text(path)
+    return show_all(text)
+
+
+def list_the_words(path):
+    """
+    lê o texto e retorna a lista de palavras dele
+    :param path: caminho do arquivo
+    :return: lista de palavras do texro
+    """
+    text = get_text(path)
+    list_words = text_clean(text).split()
+    return list_words
 
 
 def get_text(path):
@@ -163,6 +220,11 @@ def descending_alphabetical_order(list_words):
     return sorted(word_sorted, reverse=True)
 
 
+def calculate_percentage(ocurrences, total):
+    percentage_value = (ocurrences / total) * 100
+    return percentage_value
+
+
 def print_percentage(dict1, dict2):
     """
     imprime as porcentagens de aparições das palavras de cada dict
@@ -176,13 +238,13 @@ def print_percentage(dict1, dict2):
     print("Porcentagem da frequencia dos anteriores: \n")
     for item in dict1:
         ocurrences = dict1.get(item, 0)
-        percentage_value = (ocurrences / total_itens) * 100
+        percentage_value = calculate_percentage(ocurrences, total_itens)
         print(item, "(", percentage_value, "%) \n")
     print('-' * 150)
     print("Porcentagem da frequencia dos posteriores: \n")
     for item in dict2:
         ocurrences = dict2.get(item, 0)
-        percentage_value = (ocurrences / total_itens2) * 100
+        percentage_value = calculate_percentage(ocurrences, total_itens2)
         print(item, "(", percentage_value, "%) \n")
     return 0
 
@@ -197,14 +259,14 @@ def desempatar(lista):
     return escolha
 
 
-"""def print_most_commons(lista1, lista2, word):
-    ""
+def print_most_commons(lista1, lista2, word):
+    """"
     imprime a frase mais provável
     :param lista1: lista das anteriores mais freuquentes
     :param lista2: lista das posteriores mais freuquentes
     :param word: palavra escolhida pelo usuário
     :return: frase mais provável
-    ""
+    """
     print("A frase mais provável é: ")
     if len(lista1) > 1 and len(lista2) > 1:
         immediate_previous = desempatar(lista1)
@@ -219,7 +281,7 @@ def desempatar(lista):
         immediate_subsequent = desempatar(lista2)
         return f"{immediate_previous} {word} {immediate_subsequent}"
     else:
-        return f"{lista1[0]} {word} {lista2[0]}" """
+        return f"{lista1[0]} {word} {lista2[0]}"
 
 
 def check_tie(dict1, dict2, lista):
@@ -244,7 +306,6 @@ def check_tie(dict1, dict2, lista):
         print(previous_word_tie, "\n")
         print("Lista das mais frequentes posteriores:")
         print(subsequent_word_tie, "\n")
-        print('-' * 150, "\n")
     return "não tem nenhuma palavra nas listas"
 
 
@@ -265,6 +326,55 @@ def frequency(dict1, dict2, lista):
     return "não tem palavras nas listas"
 
 
+def add_in_list(lista, word, words):
+    """
+    adiciona itens em uma lista se esse item for igual a palavra escolhida pelo usuário.
+    :param lista: lista em que os itens serão adicionados
+    :param word: palavra escolhida pelo usuário
+    :param words: lista de palavras do documento
+    :return: tamanho da lista
+    """
+    for i, item in enumerate(words):
+        if item == word:
+            lista.append(i)
+    return len(lista)
+
+
+def add_itens_in_dicts(func, dict1, dict2, word, words):
+    """
+    se existir ao menos 1 item na lista, adiciona esse item na sua respectiva dict e atribui
+     valor a esses itens, que aumenta a medida que ele reaparece na contagem da lista
+    :param func: função que retorna o tamanho da lista
+    :param dict1: dicionário de palavras anteriores, que no início está vazio
+    :param dict2: dicionário de palavras posteriores, que no início está vazio
+    :param word: palavra escolhida pelo usuário
+    :param words: lista de palavras
+    :return: dicionarios de anteriores e posteriores, com os seus respectivos itens e valores,
+    que agora não estará mais vazio, como no início
+    """
+    if func > 0:
+        # Conta a frequencia das palavras anteriores e posteriores
+        for j, item in enumerate(words):
+            if item == word:
+                previous_word = words[j - 1]
+                subsequent_word = words[j + 1]
+
+                # Se a palavra já estiver no dict das anteriores, adiciona mais 1 ao seu valor
+                if previous_word in dict1:
+                    dict1[previous_word] += 1
+                # Se a palavra não estiver no dict das anteriores, adiciona ela e coloque seu valor como 1.
+                else:
+                    dict1[previous_word] = 1
+
+                # Se a palavra já estiver no dict das posteriores, adiciona mais 1 ao seu valor
+                if subsequent_word in dict2:
+                    dict2[subsequent_word] += 1
+                # Se a palavra não estiver no dict das posteriores, adiciona ela e coloque seu valor como 1.
+                else:
+                    dict2[subsequent_word] = 1
+    return dict1, dict2
+
+
 def show_all(txt):
     """
     Calcula a frequencia de aparições das palavras
@@ -277,31 +387,8 @@ def show_all(txt):
     previous = {}
     subsequent = {}
 
-    for i, item in enumerate(words):
-        if item == word:
-            lista_de_busca.append(i)
-
-    if len(lista_de_busca) > 0:
-        # Conta a frequencia das palavras anteriores e posteriores
-        for j, item in enumerate(words):
-            if item == word:
-                previous_word = words[j - 1]
-                subsequent_word = words[j + 1]
-
-                # Se a palavra já estiver no dict das anteriores, adiciona mais 1 ao seu valor
-                if previous_word in previous:
-                   previous[previous_word] += 1
-                # Se a palavra não estiver no dict das anteriores, adiciona ela e coloque seu valor como 1.
-                else:
-                    previous[previous_word] = 1
-
-                # Se a palavra já estiver no dict das posteriores, adiciona mais 1 ao seu valor
-                if subsequent_word in subsequent:
-                    subsequent[subsequent_word] += 1
-                # Se a palavra não estiver no dict das posteriores, adiciona ela e coloque seu valor como 1.
-                else:
-                    subsequent[subsequent_word] = 1
-
+    list_add = add_in_list(lista_de_busca, word, words)
+    add_itens_in_dicts(list_add, previous, subsequent, word, words)
     frequency(previous, subsequent, lista_de_busca)
     print_percentage(previous, subsequent)
     check_tie(previous, subsequent, lista_de_busca)
